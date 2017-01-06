3211ae3cd051a62ee6158c7bfce38563a12c38dd
tbrandsema <t.brandsema@gmail.com>
2017-01-06 21:59:12 +0100

    Angular Material: Using a BottomSheet Lesson 5: https://egghead.io/lessons/angularjs-angular-material-using-a-bottomsheet

--- UserController.js
+++ UserController.js
@@ -57,7 +57,7 @@
 
             $mdBottomSheet.show({
                 controllerAs  : "vm",
-                templateUrl   : './src/users/view/contactSheet.html',
+                templateUrl   : './bottomsheet.html',
                 controller    : [ '$mdBottomSheet', ContactSheetController],
                 parent        : angular.element(document.getElementById('content'))
             }).then(function(clickedItem) {
@@ -70,10 +70,10 @@
             function ContactSheetController( $mdBottomSheet ) {
                 this.user = selectedUser;
                 this.items = [
-                    { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
-                    { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
-                    { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
-                    { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
+                    { name: 'Phone'       , icon: 'phone'       , icon_url: 'svg/phone.svg'},
+                    { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'svg/twitter.svg'},
+                    { name: 'Google+'     , icon: 'google_plus' , icon_url: 'svg/google_plus.svg'},
+                    { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'svg/hangouts.svg'}
                 ];
                 this.contactUser = function(action) {
                     // The actually contact process has not been implemented...
